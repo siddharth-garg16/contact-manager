f4fbaf41caf050e16b3a1e4278895406bc6036fb
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-07-29 20:38:53 +0530

    model schema changes'

--- models/contactModel.js
+++ models/contactModel.js
@@ -3,15 +3,18 @@ const mongoose = require('mongoose');
 const contactSchema = mongoose.Schema({
     name:{
         type: String,
-        required: [true, "Please add the contact name."]
+        required: [true, "Please add the contact name."],
+        trim: true
     },
     email:{
         type: String,
-        required: [true, "Please add the contact email."]
+        required: [true, "Please add the contact email."],
+        trim: true
     },
     phone:{
         type: String,
-        required: [true, "Please add the contact number."]
+        required: [true, "Please add the contact number."],
+        trim: true
     }
 }, {
     timestamps: true
